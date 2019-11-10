f91dd3889dbda4b7879861991475ddac4207a24d
owenwalker92 <owenwalker92@gmail.com>
2019-11-09 21:25:23 -0800

    Add files via upload

--- character-config-copy.ps1
+++ character-config-copy.ps1
@@ -2,33 +2,30 @@
 $gameData = "$mydoc\My Games\FINAL FANTASY XIV - A Realm Reborn"
 $dropboxPath = Get-Content "$ENV:LOCALAPPDATA\Dropbox\info.json" -ErrorAction Stop | ConvertFrom-Json | % 'personal' | % 'path'
 $backupPath = "$dropboxPath\Backup Folder\FFXIV"
-
 $charList = gci $gamedata |? {$_.Name -like "FFXIV_CHR*"}
-
 $backupDate = "backup" + (get-date -format yyMMdd-hhmm)
-
 function make-link ($target, $link) {
     New-Item -Path $link -ItemType SymbolicLink -Value $target
 }
 
-
+#run links per character
 foreach ($c in $charList) 
 	{
         #create folder to backup if not there
         
         if (!(test-path "$gamedata\$backupDate\$c")) 
 		    {
-            write-host "THING"
             mkdir "$gamedata\$backupDate\$c"
             }
-        else {Write-Host "otherthing"}
 	    
-    #backup current items
+    #backup current config items
 	mv "$gameData\$c\*" "$gameData\$backupDate\$c" -force
-        
+    
+    #get character files and location from dropbox    
     $charLoc = $backupPath + "\" + $c
     $charFiles = gci $charLoc
-
+    
+    #make links for character specific files
     foreach ($cF in $charFiles) 
          {
          make-link "$charloc\$cF" "$gamedata\$c\$cF"
@@ -36,18 +33,21 @@ foreach ($c in $charList)
 
     $commonFiles = gci "$backupPath\COMMON\CHAR"
     
+    #make links for common files between all characters
     foreach ($comF in $commonFiles)
         {
         make-link "$backupPath\COMMON\CHAR\$comF" "$gameData\$c\$comF"
         }
 
+    #make links for computer specific UI layout
     make-link "$backupPath\$env:COMPUTERNAME\ADDON.DAT" "$gameData\$c\ADDON.DAT"
 
     }
+
+#backup character appearance data and shared macros
 mv "$gameData\FFXIV_CHARA_01.dat" "$gameData\$backupDate\" -force
 mv "$gameData\MACROSYS.DAT" "$gameData\$backupDate\" -force
 
+#make links for haracter appearance data and shared macros
 make-link "$backupPath\COMMON\FFXIV_CHARA_01.DAT" "$gameData\FFXIV_CHARA_01.DAT"
 make-link "$backupPath\COMMON\MACROSYS.DAT" "$gameData\MACROSYS.DAT"
-
-
